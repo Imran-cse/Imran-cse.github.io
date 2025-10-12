12d59079554996b8ecbf250a70db968272393e34
Imran Hossain <imran.cse.ku@gmail.com>
2025-10-12 19:11:01 +0600

    Refactor social media links and email to use constants; add constants file for better maintainability

---
+++ src/libs/constants.ts
@@ -0,0 +1,7 @@
+export const SOCIAL_MEDIA = {
+  github: "https://github.com/imran-cse",
+  linkedin: "https://linkedin.com/in/imran1503",
+  twitter: "https://twitter.com/imran728",
+};
+export const EMAIL = "imran.cse.ku@gmail.com";
+export const PHONE = "+8801681717471";
